91e12e9407df52344504621931fb78ec39943922
Kapil Vyas <kapilvyas@Kapils-MacBook-Pro.local>
2015-02-08 21:04:53 -0600

    Sideways Triangle Problem

---
+++ sideways_triangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "math.h"
+using std::cin;
+using std::cout;
+
+int main() {
+  for(int i=3;i>=-3;i--) {
+    for(int j=1;j<=4-abs(i);j++) {
+      cout << ">";
+    }
+    cout << "\n";
+  }
+  /*
+	for(int i=-3;i<=3;i++) {
+    cout << 4-abs(i) << "\n";
+	}
+  */
+  /*
+  for(int i=-3;i<=3;i++) {
+    cout << abs(i)+1 << "\n";
+	}
+  */
+	return 0;
+}
+
