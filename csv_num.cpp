bd0259a42075cf5b5e078cadbefdaca1140c10cc
Kapil Vyas <kapil.akshar@gmail.com>
2015-02-08 21:47:43 -0600

    Bug fix in Digit stream to Number Problem

--- csv_num.cpp
+++ csv_num.cpp
@@ -5,24 +5,20 @@ using std::cout;
 
 int main() {
   char digit = cin.get();
+  int prev_num = 0;
+  int number = 0;
+
   while( digit != '\n') {
-    int count = 0;
-    int number = 0;
     if( digit != ',') {
-      if( count > 1 )
-        number = number*10+(int(digit-'0'));
-      else {
-        number = int(digit-'0');
-        cout << "Count: " << count << "\n";
-      }
-      count++;
+        number = prev_num*10+(int(digit-'0'));
+        prev_num = number;
     }
     else {
       cout << "Number: " << number << "\n";
-      count = 0;
+      prev_num = 0;
     }
     digit = cin.get();
   }
+  cout << "Number: " << number << "\n";
 	return 0;
 }
-
