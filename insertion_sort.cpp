e4e8c9b03f5b212ca05dec0e6dd9c6c2d3aa0e72
Kapil Vyas <kapil.akshar@gmail.com>
2015-02-21 16:33:14 -0600

    Tried variation of loop constructs

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -3,18 +3,6 @@
 using std::cout;
 using std::string;
 
-void sort_simple(int * array, int length) {
-  for( int i = 1; i < length; i++ ) {
-    int j = i;
-    int curr = array[j];
-    while( curr < array[j-1] && j > 0) {
-      array[j] = array[j-1];
-      j--;
-    }
-    array[j] = curr;
-  }
-}
-
 void print_array(string text, int * array, int length) {
   cout << text << "\n";
   for( int i = 0; i < length; i++ ) 
@@ -22,12 +10,42 @@ void print_array(string text, int * array, int length) {
   cout << "\n";
 }
 
+void insertion_sort(int * array, int length) {
+  // With for and while
+  for( int i = 1; i < length; i++ ) {
+    int j = i;
+    int curr = array[j];
+    while( curr < array[j-1] && j > 0 ) {
+      array[j] = array[j-1];
+      j--;
+    }
+    array[j] = curr;
+  }
+}
+
+void insertion_sort_for(int * array, int length) {
+  // With two for
+  for( int i = 1; i < length; i++ ) {
+    for( int j = i; array[j-1] > array[j] && j >= 1; j-- ) {
+      int temp = array[j-1];
+      array[j-1] = array[j];
+      array[j] = temp;
+    }
+  }
+}
+
 int main() {
   int array[5] = {2, 4, 3, 1, 5};
-  int length = 5;
+  int array_length = 5;
 
-  print_array("Before:", array, length);
-  sort_simple(array, 5); 
-  print_array("After:", array, length);
+  print_array("Before:", array, array_length);
+  insertion_sort_for(array, 5); 
+  print_array("After:", array, array_length);
+  
+  int arr[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+  int arr_length = 8;
+  print_array("Before:", arr, arr_length);
+  insertion_sort(arr, 8); 
+  print_array("After:", arr, arr_length);
   return 0;
 }
