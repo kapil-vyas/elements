c8a5afa6addf7cf581e7e5db8f0c9618b580afb0
Kapil Vyas <kapil.akshar@gmail.com>
2015-02-20 22:57:59 -0600

    Unique String Problem - given constraint of not to use additional data structure

--- is_unique_string.cpp
+++ is_unique_string.cpp
@@ -6,6 +6,45 @@ using std::cin;
 using std::string;
 using std::strcpy;
 
+void print(bool value) {
+  if( value )
+    cout << "Unique\n";
+  else
+    cout << "Not Unique\n";
+}
+
+void print_array(char * array, int length) {
+  for(int i = 0; i < length; i++ )
+    cout << array[i] << " ";
+  cout << "\n";
+}
+
+bool is_unique_without_datastr(char * input, int length) {
+  // without data structure
+  // sort the array
+  int start = 0;
+  int end = length - 1;
+  for( int i = start+1; i <= end; i++ ) {
+    for( int j = i; j > start && input[j-1] > input[j]; j-- ) {
+      int temp = input[j-1];
+      input[j-1] = input[j];
+      input[j] = temp;
+    }
+  }
+  print_array(input, length);
+  
+  // check for consecutive occurences of a character
+  int curr_char = input[0];
+  for( int i = 1; i < length; i++ ) {
+    if( curr_char == input[i] )
+      return false;
+    else {
+      curr_char = input[i];
+    }
+  }
+  return true;
+}
+
 bool is_unique(char * input, int length) {
   int ascii_array[128];
   // Initialize to zero count:
@@ -25,13 +64,6 @@ bool is_unique(char * input, int length) {
   return true;
 }
 
-void print(bool value) {
-  if( value )
-    cout << "Unique\n";
-  else
-    cout << "Not Unique\n";
-}
-
 void test() {
   char input2[5] = {'H', 'e', 'l', 'i', 'o'};
   print(is_unique(input2 , 5));
@@ -49,7 +81,8 @@ int main() {
 
   char * cstr = new char[input.length()+1];
   std::strcpy(cstr, input.c_str());
-  print(is_unique(cstr, input.length()));
+  //print(is_unique(cstr, input.length()));
+  print(is_unique_without_datastr(cstr, input.length()));
 
   delete[] cstr;
   return 0;
