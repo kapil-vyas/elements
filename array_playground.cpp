97a5c09b5f55115ee7f7dd4444908ffc24a9848c
Kapil Vyas <kapil.akshar@gmail.com>
2015-02-22 22:42:35 -0600

    Identify Atomic operations on an int Array

---
+++ array_playground.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+// Array operations:
+// Assuming array is of type int
+// And the length is mutable
+// Atomic operations:
+
+// Get length of array: int length(array)
+//
+// Print array: void print(array)
+//
+// Sort array: void sort(array, algorithm)
+//
+// Find max: int max(array)
+// Note: return type is index, -1 if not found or error
+//
+// Find min: int min(array)
+// Note: return type is index, -1 if not found or error
+
+// Find element: int find(array, elem)
+// Note: return type is index, -1 if not found
+//
+// Add element to array: int add(array, elem)
+// Note: return type is the index at which element was added
+//
+// Delete element from array: int delete(array, elem)
+// Note: return type is the index of the deleted element
+//
+// Find and Replace element from array: int replace(array, find, replace)
+// Note: return type is the index at which element was found, -1 if not found
+int main() {
+  return 0;
+}
