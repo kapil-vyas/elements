98e3a12c421f888b4049a7118682777feddb5282
Kapil Vyas <kapil.akshar@gmail.com>
2015-03-06 23:40:29 -0600

    Linked List Problem - add Node using Insertion Sort

--- students.cpp
+++ students.cpp
@@ -21,6 +21,32 @@ void add(P_NODE& collection, STUDENT student) {
   collection = new_student;
 }
 
+void add_sorted(P_NODE& head, STUDENT student) {
+  P_NODE new_node = new NODE;
+  new_node->student = student;
+
+  if( head == NULL ) {
+    head = new_node;
+    head->p_next = NULL;
+    return;
+  }
+
+  P_NODE prev_node = NULL;
+  P_NODE curr_node = head;
+
+  while(curr_node->student.student_id < new_node->student.student_id) {
+    prev_node = curr_node;
+    if(curr_node->p_next == NULL) {
+      curr_node->p_next = new_node;
+      new_node->p_next = NULL;
+      return;
+    }
+    curr_node = curr_node->p_next;
+  }
+  prev_node->p_next = new_node;
+  new_node->p_next = curr_node;
+}
+
 double average(P_NODE& collection) {
   double sum = 0;
   double count = 0;
@@ -66,8 +92,35 @@ void test_add(P_NODE& collection) {
   cout << "Average: " << avge << "\n";
 }
 
+void test_add_sorted(P_NODE& collection) {
+  print(collection);
+
+  STUDENT student1 = {1, 87};
+  add_sorted(collection, student1);
+  print(collection);
+
+  STUDENT student2 = {3, 98};
+  add_sorted(collection, student2);
+  print(collection);
+ 
+  STUDENT student3 = {2, 74};
+  add_sorted(collection, student3);
+  print(collection);
+
+  STUDENT student4 = {4, 94};
+  add_sorted(collection, student4);
+  print(collection);
+
+  double avge = average(collection);
+  cout << "Average: " << avge << "\n";
+}
 int main(int argc, char *argv[]) {
-  P_NODE collection = NULL;
-  test_add(collection);
+  P_NODE collection1 = NULL;
+  cout << "Unsorted Test" << "\n";
+  test_add(collection1);
+
+  P_NODE collection2 = NULL;
+  cout << "Sorted Test" << "\n";
+  test_add_sorted(collection2);
   return 0;
 }
