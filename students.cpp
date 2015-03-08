42ba7c06b8bc8bbc25883a158a1efc10eae6ae04
Kapil Vyas <kapil.akshar@gmail.com>
2015-03-07 23:02:38 -0600

    Linked List Problem - delete Node

--- students.cpp
+++ students.cpp
@@ -47,6 +47,33 @@ void add_sorted(P_NODE& head, STUDENT student) {
   new_node->p_next = curr_node;
 }
 
+bool delete_record(P_NODE& head, int student_id) {
+  if( head == NULL ) {
+    return false;
+  }
+
+  P_NODE prev_node = NULL;
+  P_NODE curr_node = head;
+
+  while(curr_node != NULL) {
+    if(curr_node->student.student_id == student_id) {
+      if(prev_node == NULL) { 
+        delete curr_node;
+        curr_node = NULL;
+        head = NULL;
+        return true;
+      }
+      prev_node->p_next = curr_node->p_next; 
+      delete curr_node;
+      curr_node = NULL; 
+      return true;
+    }
+    prev_node = curr_node;
+    curr_node = curr_node->p_next;
+  }
+  return false;
+}
+
 double average(P_NODE& collection) {
   double sum = 0;
   double count = 0;
@@ -114,13 +141,56 @@ void test_add_sorted(P_NODE& collection) {
   double avge = average(collection);
   cout << "Average: " << avge << "\n";
 }
+
+void test_delete_record(P_NODE& collection) {
+  bool is_deleted = false;
+  print(collection);
+  is_deleted = delete_record(collection, 1);
+  if(is_deleted) cout << "Record was deleted\n";
+  else cout << "Record was not deleted as it could not be found\n";
+  print(collection);
+
+  STUDENT student1 = {1, 87};
+  add_sorted(collection, student1);
+  print(collection);
+
+  is_deleted = delete_record(collection, 1);
+  if(is_deleted) cout << "Record was deleted\n";
+  else cout << "Record was not deleted as it could not be found\n";
+  print(collection);
+
+  STUDENT student2 = {3, 98};
+  add_sorted(collection, student2);
+  print(collection);
+ 
+  STUDENT student3 = {2, 74};
+  add_sorted(collection, student3);
+  print(collection);
+
+  /*
+  is_deleted = delete_record(collection, 3);
+  if(is_deleted) cout << "Record was deleted\n";
+  else cout << "Record was not deleted as it could not be found\n";
+  print(collection);
+
+  STUDENT student4 = {4, 94};
+  add_sorted(collection, student4);
+  print(collection);
+  */
+}
+
 int main(int argc, char *argv[]) {
+
   P_NODE collection1 = NULL;
-  cout << "Unsorted Test" << "\n";
+  cout << "\nUnsorted Test" << "\n";
   test_add(collection1);
 
   P_NODE collection2 = NULL;
-  cout << "Sorted Test" << "\n";
+  cout << "\nSorted Test" << "\n";
   test_add_sorted(collection2);
+
+  P_NODE collection3 = NULL;
+  cout << "\nDelete Record Test" << "\n";
+  test_delete_record(collection3);
   return 0;
 }
