e9c22814a20b6b16c796df4a201d3dede7ca440d
Kapil Vyas <kapil.akshar@gmail.com>
2015-05-31 03:34:18 -0500

    Use strlen instead of passing array length

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -2,7 +2,8 @@
 #include <cstring>
 using namespace::std;
 
-void reverseString(char * array, int size) {
+void reverseString(char * array) {
+  int size = strlen(array)+1;
   for(int i = 0; i < (size-1)/2; i++) {
     int temp = array[i];
     array[i] = array[size-2-i];
@@ -13,7 +14,7 @@ void reverseString(char * array, int size) {
 int main() {
   // Test 1 
   char array1[5] = {'a', 'b', 'c', 'd', '\0'};
-  reverseString(array1, 5);
+  reverseString(array1);
   char array1_expected[5] = {'d', 'c', 'b', 'a', '\0'};
   if(strcmp(array1, array1_expected) == 0) {
     cout << "Test 1 Passed\n";
@@ -24,7 +25,7 @@ int main() {
 
   // Test 2
   char array2[6] = {'a', 'b', 'c', 'd', 'e', '\0'};
-  reverseString(array2, 6);
+  reverseString(array2);
   char array2_expected[6] = {'e', 'd', 'c', 'b', 'a', '\0'};
   if(strcmp(array2, array2_expected) == 0) {
     cout << "Test 2 Passed\n";
